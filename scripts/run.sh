87e47fb4844ff9f7ed5b75072b4fb8d2d95bf92e
Marcelo Braga <marcelo.braga@sixbell.com>
2022-05-25 16:05:30 -0300

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
-url=$1
-if [ -z "$url" ]
-then
-    #url="$(minikube ip):$INGRESS_PORT/customer"
-    url="curl -X PUT $(minikube ip):$INGRESS_PORT/rios/establish -d '{"ani": "21981024950", "dnis": "0800994455"}'"
-fi
-
 while true
-do curl $url
+do curl -X PUT $(minikube ip):$INGRESS_PORT/sigas/establish -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"ani\": \"21981024950\", \"dnis\": \"0800994455\"}"
 sleep .5
 done
